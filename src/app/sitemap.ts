650db9ec9177db6a8214ddcd62061fa2fd1f1f13
SandiSopian <shunasand73@gmail.com>
2026-02-11 09:31:55 +0700

    added more page for sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,30 @@
-export default function sitemap() {
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://ai-text-helper.vercel.app";
   return [
     {
-      url: "https://ai-text-helper.vercel.app/",
+      url: `${baseUrl}/`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/caption-jualan-online`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/caption-promosi-wa`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/caption-instagram`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/ringkas-teks-otomatis`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/parafrase-kalimat`,
       lastModified: new Date(),
     },
   ];
